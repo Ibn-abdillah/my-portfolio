43b966d5ce0069d2a3bba397249b94d716fd6457
Ibn-abdillah <akinolasheriff2022@gmail.com>
2023-06-09 09:30:23 +0100

    deleted some personal projects

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import estate from '../../assets/estate.png'
 import exe from '../../assets/exe.png'
-import ecommerce from '../../assets/ecommerce.png'
 import crypto from '../../assets/crypto.png'
-import barber from '../../assets/barber.png'
-import card from '../../assets/card.png'
-import interior from '../../assets/interior.png'
-import netflix from '../../assets/netflix.png'
-import twitter from '../../assets/twitter.png'
-import show from '../../assets/show.png'
-//import form from '../../assets/form.png'
-import gallery from '../../assets/gallery.png'
-//import editor from '../../assets/editor.png'
-import load from '../../assets/load.png'
-import clock from '../../assets/clock.png'
 import weather from '../../assets/weather.png'
 import coins from '../../assets/coins.png'
 
@@ -24,7 +12,7 @@ function Portfolio() {
     <div className='portfolio' id='portfolio'>
       <h2><span>My</span> Portfolio</h2>
       <div className="content">
-      <h3 className='mobile'>Built with React JS 👇</h3>
+
       <div className="project">
           <img src={crypto} alt="" />
           <h3>Trading App</h3>
@@ -73,128 +61,6 @@ function Portfolio() {
           <div className="project-btn">
             <button><a href="https://weatherfetchapi.netlify.app/">View</a></button>
             <button><a href="https://github.com/Ibn-abdillah/weather-app">Source</a></button>
-          </div>
-        </div>
-
-        <h3 className='mobile'>Built with HTML5, CSS3 and JS 👇</h3>
-
-        <div className="project">
-          <img src={ecommerce} alt="" />
-          <h3>E-commerce website</h3>
-          <p>with HTML5, CSS3, Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://e-commerce-website-one.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/E-commerce-website">Source</a></button>
-          </div>
-        </div>
-
-        <div className="project">
-          <img src={barber} alt="" />
-          <h3>Barber website</h3>
-          <p>with HTML5, CSS3, Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://barbershop-website1.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Barber-Shop-Website">Source</a></button>
-          </div>
-        </div>
-
-        <div className="project">
-          <img src={netflix} alt="" />
-          <h3>Netflix clone website</h3>
-          <p>with HTML5 and CSS3</p>
-          <div className="project-btn">
-            <button><a href="https://netflix-clone-pro.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Netflix-clone-project">Source</a></button>
-          </div>
-        </div>
-
-        <div className="project">
-          <img src={interior} alt="" />
-          <h3>Interior Design website</h3>
-          <p>with HTML5, CSS3, Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://interior-website-project.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Interior-Website">Source</a></button>
-          </div>
-        </div>
-
-        <div className="project">
-          <img src={gallery} alt="" />
-          <h3>Image Gallery</h3>
-          <p>with HTML5, CSS3 and Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://image-gallery-pro.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Image-Gallery">Source</a></button>
-          </div>
-        </div>
-
-        <div className="project">
-          <img src={card} alt="" />
-          <h3>Credit Card design</h3>
-          <p>with HTML5 and CSS3</p>
-          <div className="project-btn">
-            <button><a href="https://creditcard-1.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Virtual-Card">Source</a></button>
-          </div>
-        </div>
-
-        {/* <div className="project">
-          <img src={editor} alt="" />
-          <h3>Code Editor</h3>
-          <p>with HTML5, CSS3 and Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://codeeditor-1.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Code-Editor-App">Source</a></button>
-          </div>
-        </div> */}
-
-        <div className="project">
-          <img src={clock} alt="" />
-          <h3>Analog Clock</h3>
-          <p>with HTML5, CSS3 and Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://analog-clock-1.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Analog-clock">Source</a></button>
-          </div>
-        </div>
-
-        <div className="project">
-          <img src={load} alt="" />
-          <h3>Loading bar</h3>
-          <p>with HTML5, CSS3 and Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://loading-bar-1.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Loading-bar">Source</a></button>
-          </div>
-        </div>
-
-        <div className="project">
-          <img src={twitter} alt="" />
-          <h3>Twitter Signup Page</h3>
-          <p>with HTML5 and CSS3</p>
-          <div className="project-btn">
-            <button><a href="https://twitter-signin-page.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Twitter-SignIn-page">Source</a></button>
-          </div>
-        </div>
-
-        {/* <div className="project">
-          <img src={form} alt="" />
-          <h3>signup and signin form</h3>
-          <p>with HTML5, CSS3 and Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://form-001.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Login-Form">Source</a></button>
-          </div>
-        </div> */}
-
-        <div className="project">
-          <img src={show} alt="" />
-          <h3>Toggle Password</h3>
-          <p>with HTML5, CSS3 and Javascript</p>
-          <div className="project-btn">
-            <button><a href="https://showpassword-1.netlify.app/">View</a></button>
-            <button><a href="https://github.com/Ibn-abdillah/Toggle-password">Source</a></button>
           </div>
         </div>
       </div>
